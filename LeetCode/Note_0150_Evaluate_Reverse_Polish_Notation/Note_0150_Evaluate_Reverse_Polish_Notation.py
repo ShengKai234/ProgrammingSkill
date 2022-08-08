5ad6f0d4552aa1b182a9def800406328c7592fa9
shengkai23444 <shengkai23444@gmail.com>
2022-08-09 00:31:04 +0800

    add new problem

---
+++ LeetCode/Note_0150_Evaluate_Reverse_Polish_Notation/Note_0150_Evaluate_Reverse_Polish_Notation.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        operators = {'+', '-', '*', '/'}
+        stack = []
+        n = len(tokens)
+        
+        def cal(v1, v2, op):
+            if op == '+':
+                return v1 + v2
+            elif op == '-':
+                return v1 - v2
+            elif op == '*':
+                return v1 * v2
+            else:
+                return int(v1 / v2) # Python2 is wired
+        
+        for i in range(n):
+            if tokens[i] in operators:
+                p_right = stack.pop()
+                p_left = stack.pop()
+                stack.append(cal(int(p_left), int(p_right), tokens[i]))
+            else:   
+                stack.append(tokens[i])
+        return stack[0]
+    
+    
+    
+solution = Solution()
+print(solution.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
