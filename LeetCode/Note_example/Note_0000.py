565de5b15fe3e7b71fa46dcbf2638dab60315afc
shengkai234 <bboykai0504@gmail.com>
2023-03-22 21:20:14 +1000

    update

--- LeetCode/Note_example/Note_0000.py
+++ LeetCode/Note_example/Note_0000.py
@@ -1,8 +1,17 @@
-class Solution(object):
-    
-   
+from collections import deque
 
-solution = Solution()
+
+# class Solution(object):
+    
+
+
+# solution = Solution()
 
 # T:O(n)
 # S:O(n)
+
+for i in range(0):
+    print(i)
+
+stack = deque([0,1,2,3,4,5])
+print(stack.peek())
